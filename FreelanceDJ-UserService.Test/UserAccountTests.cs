0c2a781001b48b46424ca89d0e50d940e95a733c
Steijn Ploegmakers <461991@student.fontys.nl>
2022-12-05 15:33:17 +0100

    added test for getallusers

---
+++ FreelanceDJ-UserService.Test/UserAccountTests.cs
@@ -0,0 +1,52 @@
+﻿using FreelanceDJ_UserService.Data.Repos;
+using FreelanceDJ_UserService.Models.User;
+using Moq;
+using NUnit.Framework;
+using System.Security.Principal;
+
+namespace FreelanceDJ_UserService.Test
+{
+    public class UserAccountTests
+    {
+        private readonly List<User> _users;
+
+        public UserAccountTests()
+        {
+            _users = new List<User>
+            {
+                new()
+                {
+                    Id = new Guid(),
+                    Name = "Karen Janssen",
+                    Email = "Karenjanssen@gmail.com",
+                    GoogleId = "teewt43343trdfger2223"
+                },
+
+                new()
+                 {
+                    Id = new Guid(),
+                    Name = "Richard van Zandvoort",
+                    Email = "Richardvzandvoort@gmail.com",
+                    GoogleId = "qrgj93u383r48sfdsh4"
+                }
+            };
+        }
+
+
+        [Test]
+        public async Task GetAllDjs_ReturnListWithDjs()
+        {
+            //Arrange
+            var data = new Mock<IUserServiceRepository>();
+            data.Setup(m => m.GetAllUsers()).ReturnsAsync(_users);
+
+            var userService = new Service.UserService(data.Object);
+
+            //Act
+            var users = await userService.GetAllUserAccounts();
+
+            //Arrange
+            Assert.AreEqual(users.Count, _users.Count);
+        }
+    }
+}
--- FreelanceDJ-UserService/Service/UserService.cs
+++ FreelanceDJ-UserService/Service/UserService.cs
@@ -1,6 +1,20 @@
-﻿namespace FreelanceDJ_UserService.Service
+﻿using FreelanceDJ_UserService.Data.Repos;
+using FreelanceDJ_UserService.Models.User;
+
+namespace FreelanceDJ_UserService.Service
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        private readonly IUserServiceRepository _userServiceRepository;
+
+        public UserService(IUserServiceRepository userServiceRepository)
+        {
+            _userServiceRepository = userServiceRepository;
+        }
+
+        public async Task<List<User>> GetAllUserAccounts()
+        {
+            return await _userServiceRepository.GetAllUsers();
+        }
     }
 }
